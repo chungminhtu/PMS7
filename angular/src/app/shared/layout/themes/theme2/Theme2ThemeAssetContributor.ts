089151b01de867c972a080c2fc208de77f7750db
ultimatemm <ultimatemyanmar@gmail.com>
2019-06-17 22:52:47 +0800

    delete front end

--- angular/src/app/shared/layout/themes/theme2/Theme2ThemeAssetContributor.ts
+++
@@ -1,7 +0,0 @@
-import { IThemeAssetContributor } from '../ThemeAssetContributor';
-
-export class Theme2ThemeAssetContributor implements IThemeAssetContributor {
-    getAssetUrls(): string[] {
-        return ['/assets/fonts/fonts-asap-condensed.css'];
-    }
-}
